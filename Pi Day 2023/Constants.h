3264ae03ab7160b215c7a06de5e5f84dfb5c0971
Tom Scott <tom.scott45456@gmail.com>
2023-03-14 13:28:13 +0000

    Set up SFML window

---
+++ Pi Day 2023/Constants.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+namespace constants
+{
+	constexpr int WINDOW_WIDTH = 600;
+	constexpr int WINDOW_HEIGHT = 600;
+
+	constexpr unsigned FPS = 60;
+}
--- Pi Day 2023/Pi Day 2023.cpp
+++ Pi Day 2023/Pi Day 2023.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
+#include <SFML/Window.hpp>
+
+#include "Constants.h"
+
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	sf::Window window(sf::VideoMode(constants::WINDOW_WIDTH, constants::WINDOW_HEIGHT), "Pi Day 2023");
+
+	window.setFramerateLimit(constants::FPS);
+
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
+	return 0;
 }
 
